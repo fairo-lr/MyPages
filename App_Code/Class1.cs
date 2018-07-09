f9627d238f34149b70c5cd2883f94b922e97faa5
fairo-lr <fairoliur@gmail.com>
2018-07-09 16:25:56 +0800

    home page change

---
+++ App_Code/Class1.cs
@@ -0,0 +1,35 @@
+﻿/*
+    Function: override
+*/
+using System;
+namespace Class1
+{
+    class Employee
+    {
+        //父类虚方法
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Employee");
+        }
+
+        public void Name() { }
+    }
+
+    //子类继承重写CalculatePay()方法
+    class SalariedEmploy : Employee
+    {
+        public override void CalculatePay()
+        {
+            Console.WriteLine("Salary");
+        }
+        public new void Name() { }
+    }
+
+    class S3 : SalariedEmploy
+    {
+        public override void CalculatePay()
+        {
+            //base.CalculatePay();
+        }
+    } 
+} 
